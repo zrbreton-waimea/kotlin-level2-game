9243883b4d01b69fe422a28209a8c742fc05c6eb
Zander <zrbreton@waimea.school.nz>
2026-03-19 12:26:00 +1300

    Editing Main.kt
    I started adding the intro part to the game. Print stuff.

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,12 @@
+import java.lang.Thread.sleep
+
 /**
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
  * Project Name:   Pinned!
  * Project Author: Zander Breton
- * GitHub Repo:    
+ * GitHub Repo:    https://github.com/zrbreton-waimea/kotlin-level2-game
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE
@@ -14,6 +16,8 @@
 
 
 fun main() {
-
+    println("Welcome to Pinned!")
+    sleep(1000)
+    println("Please read through the rules before playing the game, you will need 2 players.")
 }
 
